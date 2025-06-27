9322b3bf53c9dcd206610341a6c7a378ba92297e
Josh Taylor <joshtt1303@gmail.com>
2025-06-27 13:26:01 -0700

    logo add & bigger countdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import FlipClock from './components/FlipClock';
+import cornerImage from './assets/1024_1024.png';
 
 // Target date: July 28, 2025 at 1:00 PM PST
 // Convert to UTC (PST is UTC-8, but PDT is UTC-7 during summer)
@@ -35,9 +36,12 @@ function App() {
 
   return (
     <div className="App">
+      <div className="corner-image">
+        <img src={cornerImage} alt="Corner" />
+      </div>
       <div className="dashboard-container">
         <div className="countdown-section">
-          <h1>🚀 Launch Countdown</h1>
+          <h1>Launch Countdown</h1>
           <FlipClock timeLeft={timeLeft} previousTimeLeft={previousTimeLeft} />
           <p className="launch-date">Launch Date: July 28, 2025 at 1:00 PM PST</p>
         </div>
